7d52316592ae03626563177bdd7e6d29d44f3b7c
SrinathDev <srinath.a@techjays.com>
2022-12-23 14:04:13 +0530

    space deleted

--- app/src/main/java/com/example/githflowbuild/MainActivity.kt
+++ app/src/main/java/com/example/githflowbuild/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         Toast.makeText(this,"Github actions",Toast.LENGTH_SHORT).show()
     }
 }
