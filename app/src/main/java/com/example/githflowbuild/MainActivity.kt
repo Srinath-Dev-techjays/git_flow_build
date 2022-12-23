f8ee63559b846dba48bbc9b8bc337b1a2d74185e
SrinathDev <srinath.a@techjays.com>
2022-12-23 13:18:47 +0530

    toast added

--- app/src/main/java/com/example/githflowbuild/MainActivity.kt
+++ app/src/main/java/com/example/githflowbuild/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.githflowbuild
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Github actions",Toast.LENGTH_SHORT).show()
     }
 }
