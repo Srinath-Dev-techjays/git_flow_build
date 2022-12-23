2e6a566a6721b5b203575a9b73e5952de651b399
SrinathDev <srinath.a@techjays.com>
2022-12-23 13:28:39 +0530

    space

--- app/src/main/java/com/example/githflowbuild/MainActivity.kt
+++ app/src/main/java/com/example/githflowbuild/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         Toast.makeText(this,"Github actions",Toast.LENGTH_SHORT).show()
     }
 }
