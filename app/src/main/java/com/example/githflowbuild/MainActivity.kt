d809799a00a8bc5890b33a08066ad5ddb19808dd
SrinathDev <srinath.a@techjays.com>
2023-01-02 12:13:05 +0530

    git flow code changes java version

--- app/src/main/java/com/example/githflowbuild/MainActivity.kt
+++ app/src/main/java/com/example/githflowbuild/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         Toast.makeText(this,"Github actions",Toast.LENGTH_SHORT).show()
     }
 }
